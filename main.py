e438997aa7b5cc6e899aab71fdcaed2cec0d99c8
Jetstream <bigalex934@gmail.com>
2021-12-11 20:11:37 +0300

    Fix cors. Updates requirements

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 from dev.configuration_loader import load_configuration
 from v1.api import get_api_v1
+from fastapi.middleware.cors import CORSMiddleware
 
 load_configuration()
+
+# Delete in production
+origins = [
+    "*"
+]
+
 application = get_api_v1()
+application.add_middleware(CORSMiddleware, allow_origins=origins, allow_credentials=True, allow_methods=["*"],
+                           allow_headers=["*"])
+
